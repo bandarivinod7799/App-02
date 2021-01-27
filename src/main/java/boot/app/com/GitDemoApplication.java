40da72bba0a233f12b11db3babae9ec25906d353
Bandari Vinod <vinodbandari55@gmail.com>
2021-01-27 10:22:30 +0530

    method is added

--- src/main/java/boot/app/com/GitDemoApplication.java
+++ src/main/java/boot/app/com/GitDemoApplication.java
@@ -9,6 +9,10 @@ public class GitDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
 String name="AshokIT";
+
+public void info(){
+System.out.println(""we are ashok sir Students);
+}
 	}
 
 }
